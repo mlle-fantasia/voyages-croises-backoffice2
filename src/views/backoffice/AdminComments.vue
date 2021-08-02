39d41fd8375547799fe18667668e586b16899414
Marina Front <marinafront@hotmail.fr>
2021-08-02 21:47:08 +0200

    les commentaires

--- src/views/backoffice/AdminComments.vue
+++ src/views/backoffice/AdminComments.vue
@@ -1,21 +1,97 @@
 <template>
   <div>
     <h2>Les commentaires</h2>
-    <h5>{{ comments.length }} commentaires non publiés</h5>
+    <h5>{{ commentsNotVisible.length }} commentaires non publiés</h5>
     <div class="row">
-      <div class="col-md-6" v-for="com in comments" :key="com.id">
+      <div
+        class="col-md-12 col-lg-6"
+        v-for="com in commentsNotVisible"
+        :key="com.id"
+      >
         <div class="card-comment">
-          <div>Nom : {{ com.name }}</div>
-          <div>Email : {{ com.email }}</div>
-          <div>Titre : {{ com.title }}</div>
-          <div>Commentaire : {{ com.contenu }}</div>
-          <div>Site web : {{ com.sitweb }}</div>
-          <button @click="hideOrShow(com)" class="btn btn-primary btn-sm">
+          <div><span class="fw-bold">Nom :</span> {{ com.name }}</div>
+          <div><span class="fw-bold">Email :</span> {{ com.email }}</div>
+          <div><span class="fw-bold">Titre :</span> {{ com.title }}</div>
+          <div>
+            <span class="fw-bold">Commentaire :</span> {{ com.contenu }}
+          </div>
+          <div><span class="fw-bold">Site web : </span> {{ com.sitweb }}</div>
+          <div>
+            <span class="fw-bold">Date :</span> {{ com.createdAt | formatDate }}
+          </div>
+          <div>
+            <span class="fw-bold">Article :</span> {{ com.articles.title }}
+          </div>
+          <button @click="hideOrShow(com)" class="btn btn-success btn-sm">
             {{ com.visible ? "Masquer" : "Publier" }}
           </button>
           <button @click="deleteCom(com)" class="btn btn-danger btn-sm ms-2">
             Supprimer
           </button>
+        </div>
+      </div>
+    </div>
+    <h5 class="mt-4">Tous les commentaires par article</h5>
+    <div class="row">
+      <div
+        class="col-md-12 col-lg-6 mt-2"
+        v-for="article in articlesComment"
+        :key="article.id"
+      >
+        <div class="d-md-flex">
+          <div
+            class="bg-image miniature d-none d-md-block"
+            :style="getImage(article)"
+          ></div>
+          <div>
+            <div class="ms-2 fw-bold">
+              {{ article.title }}
+            </div>
+            <button
+              @click="showComments(article.id)"
+              class="btn btn-warning btn-sm ms-2"
+            >
+              Les commentaires
+            </button>
+          </div>
+          <div class="ms-2">{{ article.comments.length }} commentaire(s)</div>
+        </div>
+        <div v-if="hideOrShowAllComment(article.id)">
+          <div
+            class="comArticle ms-md-3"
+            v-for="com in article.comments"
+            :key="com.id"
+          >
+            <div>{{ com.name }} le {{ com.createdAt | formatDate }}</div>
+            <div>
+              <span class="fw-bold">{{ com.title }}</span>
+            </div>
+            <div>{{ com.contenu }}</div>
+            <div>
+              <span class="fw-bold">Email :</span> {{ com.email }}
+              <span class="ms-2" v-if="com.sitweb"
+                >Site web : {{ com.sitweb }}</span
+              >
+            </div>
+            <div class="d-flex mt-2 align-items-center">
+              <div :class="com.visible ? 'text-success' : 'text-danger'">
+                {{ com.visible ? "Visible" : "Invisible" }}
+              </div>
+              <button
+                @click="hideOrShow(com)"
+                class="btn btn-sm ms-2"
+                :class="com.visible ? 'btn-warning' : 'btn-success'"
+              >
+                {{ com.visible ? "Masquer" : "Publier" }}
+              </button>
+              <button
+                @click="deleteCom(com)"
+                class="btn btn-danger btn-sm ms-2"
+              >
+                Supprimer
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -27,7 +103,9 @@ export default {
   components: {},
   data() {
     return {
-      comments: [],
+      commentsNotVisible: [],
+      articlesComment: [],
+      showAllComment: 0,
     };
   },
   async mounted() {
@@ -35,11 +113,19 @@ export default {
       process.env.VUE_APP_SERVER_URL + "/admin/comments"
     );
     console.log(response);
-    this.comments = response.data;
+    this.commentsNotVisible = response.data.notVisible;
+    this.articlesComment = response.data.allComments;
   },
   methods: {
     getImage(article) {
       return `background-image:url('${process.env.VUE_APP_SERVER_URL}/articles/${article.id}/image')`;
+    },
+    hideOrShowAllComment(articleId) {
+      return this.showAllComment === articleId;
+    },
+    showComments(articleId) {
+      if (this.showAllComment === articleId) this.showAllComment = 0;
+      else this.showAllComment = articleId;
     },
     async hideOrShow(comment) {
       await this.$axios.put(
@@ -59,6 +145,13 @@ export default {
 .card-comment {
   background-color: #fff;
   border-radius: 15px;
+  border: 1px solid #eae8e8;
+  box-shadow: lightgrey 0px 1px 2px 0px;
+  margin-bottom: 15px;
+  padding: 5px 12px;
+}
+.comArticle {
+  margin-top: 10px;
   border: 1px solid #eae8e8;
   box-shadow: lightgrey 0px 1px 2px 0px;
   margin-bottom: 15px;
